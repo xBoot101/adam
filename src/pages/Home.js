eff9a531d8c5cc3dd3676f312ab12c117b423f48
Abul Kalam Azad <me.azad99@gmail.com>
2022-08-29 22:08:57 +0600

    Work home page.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { SearchOutlined } from '@ant-design/icons';
+import { SearchOutlined, SmileOutlined } from '@ant-design/icons';
 import { Button, Tooltip, Space } from 'antd';
+import './Home.scss';
 const Home = () => {
 	return (
-		<div>
+		<div className='container'>
 			<h2>Home page</h2>
-
+			<Space>
+				<SmileOutlined style={{ color: 'red' }} />
+			</Space>
 			<h4>Button uses ant design</h4>
 			<Space>
 				<Tooltip title='search'>

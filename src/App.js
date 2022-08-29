951eccdc890a52ef24b92dd295754a73afe24010
Abul Kalam Azad <me.azad99@gmail.com>
2022-08-29 15:48:00 +0600

    React router and ant design added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.min.css';
+import { Routes, Route } from 'react-router-dom';
 
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Navbar></Navbar>
+
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/login' element={<Login />} />
+				<Route path='/signup' element={<Signup />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+	let activeStyle = {
+		textDecoration: 'none',
+		color: 'red',
+	};
+
+	return (
+		<nav className='navigation'>
+			<ul>
+				<li>
+					<NavLink
+						to='/'
+						style={({ isActive }) =>
+							isActive ? activeStyle : undefined
+						}
+					>
+						Home
+					</NavLink>
+				</li>
+				<li>
+					<NavLink
+						to='/login'
+						style={({ isActive }) =>
+							isActive ? activeStyle : undefined
+						}
+					>
+						Login
+					</NavLink>
+				</li>
+				<li>
+					<NavLink
+						to='/signup'
+						style={({ isActive }) =>
+							isActive ? activeStyle : undefined
+						}
+					>
+						Sign up
+					</NavLink>
+				</li>
+			</ul>
+		</nav>
+	);
+};
+
+export default Navbar;
